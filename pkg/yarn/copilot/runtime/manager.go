ecc21e63ad7eb92e9542f744de85b598a14b1328
suozengzeng <suozengzeng@xiaohongshu.com>
2023-07-17 15:00:21 +0800

    fix: make lint

--- pkg/yarn/copilot/runtime/manager.go
+++ pkg/yarn/copilot/runtime/manager.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package runtime
 
 import (
@@ -28,10 +44,10 @@ func NewManger(socketDir string) *Manger {
 
 func (m *Manger) Run(stopCh <-chan struct{}) error {
 	defer runtime.HandleCrash()
-	tick := time.Tick(time.Minute)
+	tick := time.NewTicker(time.Minute)
 	for {
 		select {
-		case <-tick:
+		case <-tick.C:
 			if err := m.run(); err != nil {
 				klog.Warning(err)
 			}
@@ -45,15 +61,14 @@ func (m *Manger) run() error {
 
 	klog.V(4).Info("watch socket path %s", m.SocketDir)
 	err := filepath.Walk(m.SocketDir, func(path string, info fs.FileInfo, err error) error {
-
 		if info.Mode().Type() != fs.ModeSocket {
 			klog.V(4).Infof("%s is not socket", path)
 			return nil
 		}
 		p := newHttpPlugin(path)
-		pluginInfo, err := p.Info()
-		if err != nil {
-			return err
+		pluginInfo, err1 := p.Info()
+		if err1 != nil {
+			return err1
 		}
 		klog.Infof("discover plugin %s", pluginInfo.Name)
 		m.mtx.Lock()
